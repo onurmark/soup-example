aec6b00201c587faba276af7bb04c20195d5d336
Jace Young <onurmark1@gmail.com>
2015-04-02 14:31:32 +0900

    Initial commit

---
+++ src/soup_example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glib.h>
+#include <libsoup/soup.h>
+
+
+int main(void)
+{
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
